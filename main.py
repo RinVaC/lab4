afa61f1ea7bc13c4e1da739b169e8c0fc858d064
Alex <gwhignw@gmail.com>
2023-03-18 11:26:03 +0300

    completed commit

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-
 class Vector(object):
-    def __init__(self, vec1=[2,3,4], vec2=[4,9,8]):
-        self.vector1 = vec1
-        self.vector2 = vec2
-    def __add__(self):
-        return self.vector1 + self.vector2
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-    def __sub__(self):
-        return self.vector1 - self.vector2
+    def sum(self, vector):
+        self.x += vector.x
+        self.y += vector.y
+        self.z += vector.z
+        return self.x, self.y, self.z
 
-    def __mul__(self):
-        return self.vector1 * self.vector2
+    def sub(self, vector):
+        self.x -= vector.x
+        self.y -= vector.y
+        self.z -= vector.z
+        return self.x, self.y, self.z
 
-    def
+    def mul(self, vector):
+        self.x *= vector.x
+        self.y *= vector.y
+        self.z *= vector.z
+        return self.x, self.y, self.z
+
+    def skal(self, vector):
+        return self.x * vector.x + self.x * vector.y + self.x * vector.z \
+        + self.y * vector.x + self.y * vector.y + self.y * vector.z \
+        + self.z * vector.x + self.z * vector.y + self.z * vector.z
+
+    def vek(self, vector):
+        self.x = self.y * vector.z - self.z * vector.y
+        self.y = self.x * vector.z - self.z * vector.x
+        self.z = self.x * vector.y - self.y * vector.x
+        return self.x, self.y, self.z
 
 
-    list1 = [2, 3, 4]
-    list2 = [4, 9, 8]
 
-    print(__add__)
+Vector1 = Vector(2, 3, 4)
+Vector2 = Vector(4, 9, 8)
 
-    #print('First Vector   : '+ str(vector1))
-    #print('Second Vector   : '+ str(vector2))
-    #addition = vector1 + vector2
-    #print('Vector Addition   : '+ str(addition))
-    #substraction = vector1 - vector2
-    #print('Vector Substraction   : ' + str(substraction))
-    #multiplication = vector1 * vector2
-    #print('Vector Multiplication   : ' + str(multiplication))
-    #division = vector1 / vector2
-    #print('Vector Division   : ' + str(division))
+Vector1.sum(Vector2)
+print((Vector1.x, Vector1.y, Vector1.z))
 
+Vector1.sub(Vector2)
+print((Vector1.x, Vector1.y, Vector1.z))
 
+Vector1.mul(Vector2)
+print((Vector1.x, Vector1.y, Vector1.z))
+
+print(Vector1.skal(Vector2))
+print(Vector1.vek(Vector2))
