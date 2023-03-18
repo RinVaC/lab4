062ea5e3c053b9b5294167193da8a25fd942dd3e
Alex <gwhignw@gmail.com>
2023-03-18 10:05:53 +0300

    third commit

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-print('hello wolrd')
 
-print('after commit')
+class Vector(object):
+    def __init__(self, vec1=[2,3,4], vec2=[4,9,8]):
+        self.vector1 = vec1
+        self.vector2 = vec2
+    def __add__(self):
+        return self.vector1 + self.vector2
+
+    def __sub__(self):
+        return self.vector1 - self.vector2
+
+    def __mul__(self):
+        return self.vector1 * self.vector2
+
+    def
+
+
+    list1 = [2, 3, 4]
+    list2 = [4, 9, 8]
+
+    print(__add__)
+
+    #print('First Vector   : '+ str(vector1))
+    #print('Second Vector   : '+ str(vector2))
+    #addition = vector1 + vector2
+    #print('Vector Addition   : '+ str(addition))
+    #substraction = vector1 - vector2
+    #print('Vector Substraction   : ' + str(substraction))
+    #multiplication = vector1 * vector2
+    #print('Vector Multiplication   : ' + str(multiplication))
+    #division = vector1 / vector2
+    #print('Vector Division   : ' + str(division))
+
+
